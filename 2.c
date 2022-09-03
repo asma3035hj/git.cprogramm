1a13c2c8400ad9cffe08c68326ae4cb4f8a18f00
asma-talukder <asmatalukser545@gmail.com>
2022-09-03 16:07:06 +0600

    some practice c programm loop-for whilw do while

---
+++ 2.c
@@ -0,0 +1,13 @@
+//for while do while
+#include<stdio.h>z
+int main()
+{
+    int i=1;
+    do
+        {
+          printf("c programming for while do while practice\n");
+          printf("%d\n",i);
+          i++;
+        }while(i<=10);
+   return 0;
+}
